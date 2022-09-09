7ac76475d69e3d1c7a690bc8525330c5f793ad1c
Elias Faith <elias.faith27@gmail.com>
2022-09-08 19:24:59 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - program start and ends in this function
+ * takes no arguments
  *
- *  * main - program start and ends in this function
- *
- *   * takes no arguments
- *
- *    *
- *
- *     * Description: contains a puts() function to print string to standard output
- *
- *      * Return: function returns 0
- *
- *       */
+ * Description: contains a puts() function to print string to standard output
+ * Return: function returns 0
+ */
 
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	return (0);
 }
