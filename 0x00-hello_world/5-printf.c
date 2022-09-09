b63bc982083bdbf909a5ec0c05f7507c1c69456e
Elias Faith <elias.faith27@gmail.com>
2022-09-08 17:41:15 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * takes no arguments
+ *
+ * Description: prints a string to standard to standard output using printf
+ * Return: return 0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcome isa piece of art,\n");
+	return (0);
