d84e791c6c40e541e68367790078ce1b76733714
Elias Faith <elias.faith27@gmail.com>
2022-09-08 18:27:32 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * no argument
+ * Description: prints string to standard output
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multlingual puzzle");
+	return (0);
+}
