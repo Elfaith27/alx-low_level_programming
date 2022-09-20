2a11f2a18f3b9c0ac8f04c400d65990da0965526
Elias Faith <elias.faith27@gmail.com>
2022-09-20 15:32:25 -0700

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_rev - reverse a string
@@ -11,7 +12,7 @@ void print_rev(char *s)
 
 	for (i = _strlen(s) - 1; i >= 0; i--)
 	{
-		_putchar(*(s + i));
+		putchar(*(s + i));
 	}
-	_putchar('\n');
+	putchar('\n');
 }
